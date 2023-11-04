f99878d2595f97d92e6ec1feab26227aa1870002
Santiago Arévalo <santiagoarevalo2003@gmail.com>
2023-11-04 18:01:56 -0500

    feat: questions 4 and 5 completed, with successful tests

--- part1/question4.py
+++ part1/question4.py
@@ -24,7 +24,11 @@ import pets_db
 
 sql_pets_owned_by_nobody = """
 
-Your SQL here.
+SELECT a.name, a.species, a.age
+FROM animals a
+LEFT JOIN people_animals pa ON a.animal_id = pa.pet_id
+WHERE pa.owner_id IS NULL;
+
 
 """
 
@@ -34,7 +38,12 @@ Your SQL here.
 
 sql_pets_older_than_owner = """
 
-Your SQL here.
+SELECT COUNT(*) AS pets_older_than_owner
+FROM people_animals pa
+JOIN animals a ON pa.pet_id = a.animal_id
+JOIN people p ON pa.owner_id = p.person_id
+WHERE a.age > p.age;
+
 
 """
 
@@ -43,6 +52,17 @@ Your SQL here.
 # The output should be a list of tuples in the format: (<person name>, <pet name>, <species>)
 sql_only_owned_by_bessie = """ 
 
-Your SQL here.
+SELECT p.name AS person_name, a.name AS pet_name, a.species
+FROM people_animals pa
+JOIN people p ON pa.owner_id = p.person_id
+JOIN animals a ON pa.pet_id = a.animal_id
+WHERE p.name = 'bessie'
+  AND NOT EXISTS (
+    SELECT 1
+    FROM people_animals pa2
+    WHERE pa2.pet_id = pa.pet_id
+      AND pa2.owner_id != p.person_id
+  );
+
 
 """
